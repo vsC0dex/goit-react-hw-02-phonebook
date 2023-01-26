51e7d95010e872f0a892d9fe2f885c5a6dac06ee
vsC0dex <max071010@gmail.com>
2023-01-26 21:29:08 +0200

    add-components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,11 @@
-import React, { Component } from 'react';
-export default class App extends Component {
-  state = {
-    contacts: [],
-    name: '',
-  };
+import PhoneBook from './Phonebook/Phonebook';
 
-  render() {
-    return (
-      <div>
-        <form action="submit">
-          <label htmlFor="name">
-            <input
-              placeholder="Type something"
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-            />
-          </label>
-        </form>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <PhoneBook />
+    </div>
+  );
 }
+
+export default App;
